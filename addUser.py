c37f35206cb607102d408e21e892f0a7ac810753
Phil Wilkins <phil@mp3monster.org>
2021-05-26 23:17:18 +0100

    Added logic for quota
    simplified main

--- addUser.py
+++ addUser.py
@@ -13,10 +13,16 @@ from oci.config import from_file
 # https://github.com/oracle/oci-python-sdk/blob/master/examples/quotas_example.py -- quotas example update
 # https://docs.oracle.com/en-us/iaas/Content/Search/Concepts/querysyntax.htm - search query syntax
 
-connection_properties = None
+config_props = None
+quota_props = None
+config_filename = None
+quota_config_filename = None
+
 identity = None
 
 app_description = "automated user setup by Python SDK"
+    
+TENANCY = "tenancy"
 
 USER = "users"
 COMPARTMENT="compartments"
@@ -30,25 +36,48 @@ query_dictionary = {
   COMPARTMENT: {ALL : "query compartment resources where inactiveStatus = 0", NAMED : "query compartment resources where displayname = "},
   GROUP: {ALL : "query group resources where inactiveStatus = 0", NAMED : "query group resources where displayname = "},
   POLICY: {ALL : "query policy resources where inactiveStatus = 0", NAMED : "query policy resources where displayname = "}
-
 }
 
+QUOTA_PREFIX = "quota_"
+QUOTA_PRE_LEN = len(QUOTA_PREFIX)
 
-
-def init(*args):
-  global connection_properties
-  filename = "connection.properties"
+def init_config_filename (*args):
+  global config_filename, quota_config_filename
+  config_filename = "connection.properties"
+  quota_config_filename = "connection.properties"
 
   for arg in sys.argv[1:]:
     arg_elements = arg.split("=")
     if (arg_elements[0]=="config"):
-      filename= arg_elements[1]
-      break
+      config_filename= arg_elements[1]
+      print ("config file >" + config_filename + "<") 
+    elif (arg_elements[0]=="quotaconfig"):
+      quota_config_filename= arg_elements[1]
+      print ("quota config file >" + config_filename + "<") 
 
-  print ("config file >" + filename + "<")   
 
-  connection_properties = from_file(file_location=filename)     
-  oci.config.validate_config(connection_properties)
+def init_connection():
+  global config_props, identity
+  config_props = from_file(file_location=config_filename)     
+  oci.config.validate_config(config_props)
+  identity = oci.identity.IdentityClient(config_props)
+
+def get_quota_statements (compartmentname):
+  global quota_props, config_props
+  quota_statements = []
+
+  for config in config_props:
+    if (config.startswith(QUOTA_PREFIX)):
+      quota_name = config[QUOTA_PRE_LEN:]
+      stmt = "Set " + quota_name + " to " + config_props[config] + " in compartment " + compartmentname
+      print (stmt)
+      quota_statements.append(stmt)
+    else:
+      print ("ignoring " + config)
+  return quota_statements
+
+
+
 
 def find(name=None, query_type=USER):
   found_id = None
@@ -57,7 +86,7 @@ def find(name=None, query_type=USER):
     query = query_dictionary[query_type][NAMED]+"'" + name + "'"
     print (query)
 
-  search_client = oci.resource_search.ResourceSearchClient(connection_properties)
+  search_client = oci.resource_search.ResourceSearchClient(config_props)
   structured_search = oci.resource_search.models.StructuredSearchDetails(query=query,
                                                                            type='Structured',
                                                                            matching_context_type=oci.resource_search.models.SearchDetails.MATCHING_CONTEXT_TYPE_NONE)
@@ -79,13 +108,6 @@ def find(name=None, query_type=USER):
   return found_id
 
 
-
-def connect ():
-  global connection_properties, identity
-  identity = oci.identity.IdentityClient(connection_properties)
-
-
-
 def create_user (username, compartment_id, email):
   user_id = None
   try:
@@ -105,8 +127,8 @@ def create_user (username, compartment_id, email):
 
   return user_id
 
-def link_user_group (user, group):
-  print ("tbd")
+
+
 
 def create_compartment (parentcompartment, compartmentname):
   compartment_id = None
@@ -120,7 +142,7 @@ def create_compartment (parentcompartment, compartmentname):
     compartment_id = compartment.data.id
 
     print ("waiting on compartment state")
-    client = oci.core.ComputeClient(connection_properties)
+    client = oci.core.ComputeClient(config_props)
     oci.wait_until(client, client.get_instance(compartment_id), 'lifecycle_state', 'ACTIVE')    
   except oci.exceptions.ServiceError as se:
     print ("ERROR - Create Compartment: ")
@@ -151,7 +173,7 @@ def create_user_compartment_policies (groupname, policyname, compartment_id, com
 def create_group (groupname):
   try:
     request = oci.identity.CreateGroupDetails()
-    request.compartment_id = connection_properties["tenancy"]
+    request.compartment_id = config_props["tenancy"]
     request.name = groupname
     request.description = app_description
     group = identity.create_group(request)
@@ -161,6 +183,21 @@ def create_group (groupname):
     print (se)
   return group.data.id
 
+
+def create_compartment_quota (quota_statements, compartment_id, quotaname):
+  try:
+    request = oci.limits.models.CreateQuotaDetails()
+    request.compartment_id = compartment_id
+    request.statements = quota_statements
+    request.description = app_description
+    request.name = quotaname
+    client = oci.limits.QuotasClient(config_props)
+
+    quota = oci.limits.QuotasClient.create_quota(client,request)
+  except oci.exceptions.ServiceError as se:
+    print ("ERROR - Create Quota: ")
+    print (se)
+  return quota.data.id
 
 def username_to_oci_compatible_name(username):
   username = username.replace(".com", "")
@@ -179,6 +216,24 @@ def tostring (object):
   
   return result
 
+def get_username(username):
+  username = username_to_oci_compatible_name(username)
+
+  if (username == None):
+    username = config_props["new-username"]
+    username_to_oci_compatible_name(username)
+    #ToDo: add logic that says if empty string or None then throw error
+  return username
+
+def het_parent_compartment_ocid(teamname):
+  parent_compartment_ocid = None
+  if (teamname != None):
+    parent_compartment_ocid = find (teamname, COMPARTMENT)
+    if (parent_compartment_ocid == None):
+      raise LookupError ("No compartment found")
+  else:
+    parent_compartment_ocid = config_props[TENANCY]
+  return parent_compartment_ocid
 
 def main(*args):
   print (args)
@@ -199,38 +254,35 @@ def main(*args):
     elif (arg_elements[0]=="email"):
       email_address= arg_elements[1]
       print ("Email  >" + email_address + "<")
-  init(args)
-  connect()
-  username = username_to_oci_compatible_name(username)
 
-  if (username == None):
-    username = connection_properties["new-username"]
-    username_to_oci_compatible_name(username)
+
+  init_config_filename(args)
+  init_connection()
+
+  username = get_username(username)
     #ToDo: add logic that says if empty string or None then throw error
 
   groupname = username+"-grp"
   compartmentname = username+"-cmt"
   policyname = username+"-pol"
+  quotaname = username+"-qta"
 
   search_only = False
 
   # find()
-  print ("located " + username + " ocid=" + tostring(find(username, USER)))
-  print ("located " + compartmentname + " ocid=" + tostring(find(compartmentname, COMPARTMENT)))
-  print ("located " + groupname + " ocid=" + tostring(find(groupname, GROUP)))
-  print ("located " + policyname + " ocid=" + tostring(find(policyname, POLICY)))
+  LOCATEDMSG = "located "
+  OCIDMSG = " ocid="
+  print (LOCATEDMSG + username + OCIDMSG + tostring(find(username, USER)))
+  print (LOCATEDMSG + compartmentname + OCIDMSG + tostring(find(compartmentname, COMPARTMENT)))
+  print (LOCATEDMSG + groupname + OCIDMSG + tostring(find(groupname, GROUP)))
+  print (LOCATEDMSG + policyname + OCIDMSG + tostring(find(policyname, POLICY)))
 
-  
+  get_quota_statements(compartmentname)
+
 
   if (search_only == False):
 
-    parent_compartment_ocid = None
-    if (teamname != None):
-      parent_compartment_ocid = find (teamname, COMPARTMENT)
-      if (parent_compartment_ocid == None):
-        raise LookupError ("No compartment found")
-    else:
-      parent_compartment_ocid = connection_properties["tenancy"]
+    parent_compartment_ocid = het_parent_compartment_ocid(teamname)
 
     compartment_ocid = find(compartmentname, COMPARTMENT)
     if (compartment_ocid == None):
@@ -242,14 +294,16 @@ def main(*args):
 
     user_ocid = find(username, USER)
     if (user_ocid == None):
-      create_user (username, connection_properties["tenancy"],email_address)
+      create_user (username, config_props[TENANCY],email_address)
 
     policyname_ocid = find (policyname, POLICY)
     if (policyname_ocid== None):
       create_user_compartment_policies (groupname, policyname, compartment_ocid, compartmentname)
 
-    # link user and group
-    #set policies on compartment
+    create_compartment_quota (get_quota_statements(compartmentname),config_props[TENANCY],quotaname)
+
+
+
 
 
 if __name__ == "__main__":
