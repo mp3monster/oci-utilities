65a995072d2802d12784c5ef0f11a655e5f1403b
Phil Wilkins <phil@mp3monster.org>
2021-05-27 12:35:25 +0100

    Added additional meta data and dependencies

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setuptools.setup(
     description="A package for rapidly setting up developer users on OCI",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords='OCI Oracle Cloud Developers configuration',
     url="https://github.com/mp3monster/oci-utilities",
     project_urls={
         "Bug Tracker": "https://github.com/mp3monster/oci-utilities/issues",
@@ -19,8 +20,12 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache 2 License",
         "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
     ],
     package_dir={"": "."},
     packages=setuptools.find_packages(where=","),
-    python_requires=">=3.6",
+    python_requires=">=3.9",
+    install_requires=['oci'],
+    package_data={'sample': ['example-connection.properties'],},
 )
